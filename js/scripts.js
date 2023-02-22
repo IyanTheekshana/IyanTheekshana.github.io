4acd705897079af165aa576fa399f99166d89ebc
Iyan Theekshana <iyan.dev123@gmail.com>
2023-02-22 22:37:09 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$("#hamburger").click(function () {
+  $("#bar").toggleClass("active");
+  $("#nav__links").toggleClass("display-menu");
+});
+
+//navbar active class on scrolling
+const li = document.querySelectorAll(".nav-link");
+const sec = document.querySelectorAll(".main-section");
+
+function activemenu() {
+  let len = sec.length;
+  while (--len && window.scrollY + 100 < sec[len].offsetTop) {}
+  li.forEach((ltx) => ltx.classList.remove("active"));
+  li[len].classList.add("active");
+}
+activemenu();
+window.addEventListener("scroll", activemenu);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide", {
+    type: "loop",
+    perPage: 5,
+
+    autoplay: true,
+    arrows: false,
+    pagination: false,
+    cloneStatus: true,
+    interval: 2000,
+    gap: "1em",
+    drag: true,
+    focus: "center",
+    breakpoints: {
+      640: {
+        perPage: 2,
+        gap: "0.5em",
+      },
+      480: {
+        perPage: 1,
+        gap: "0em",
+      },
+    },
+  });
+  splide.mount();
+});
